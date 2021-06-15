50b787aed888df841ac6e10045e12789fb75ef0f
Andrii Lypovetskyi <li_andr@ukr.net>
2021-06-15 23:44:05 +0300

    Update test

--- js/scriptTest.js
+++ js/scriptTest.js
@@ -1,27 +1,85 @@
-const numbers = [1, 2, 3];
-
-//
-// // Классический for
-// for (let i = 0; i < numbers.length; i += 1) {
-//   console.log(numbers[i]);
-// }
-
-// Функциональный forEach
-numbers.forEach(num => console.log(num));
-
-// Указываем параметр idx если нужен доступ к счетчику
-numbers.forEach((num, idx) => console.log(`index ${idx}, value ${num}`));
-
-const doubledNumbers = numbers.map(num => num * 2);
-console.log(doubledNumbers); // [2, 4, 6]
-
-const users = [
-  { name: 'Mango', isActive: true },
-  { name: 'Poly', isActive: false },
-  { name: 'Ajax', isActive: true },
+const people = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+    gender: 'female',
+    age: 34,
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    skills: ['adipisicing', 'irure', 'velit'],
+    gender: 'female',
+    age: 21,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    skills: ['ex', 'culpa', 'nostrud'],
+    gender: 'male',
+    age: 27,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    skills: ['lorem', 'veniam', 'culpa'],
+    gender: 'female',
+    age: 39,
+  },
 ];
 
-// Для каждого элемента коллекции (user) вернем значение поля name
-const names = users.map(user => user.name);
 
-console.log(names); // ["Mango", "Poly", "Ajax"]
+const getUsersWithEyeColor = (users,color) => users.filter(user => user.eyeColor === color);
+
+// Пиши код выше этой строки
+console.log(getUsersWithEyeColor(people, 'green'));
