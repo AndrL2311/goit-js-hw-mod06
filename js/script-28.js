15792f9b61345879591f3d6303735fa3dacd2ac1
Andrii Lypovetskyi <li_andr@ukr.net>
2021-06-16 17:01:33 +0300

    Task28-29

--- js/script-28.js
+++ js/script-28.js
@@ -33,3 +33,17 @@
 // Объявлена переменная anyElementInThirdIsOdd.
 // Значение переменной anyElementInThirdIsOdd это буль true.
 // Для перебора массивов использован метод some().
+
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Пиши код ниже этой строки
+
+const anyElementInFirstIsEven = firstArray.some(element => element % 2 === 0);
+const anyElementInFirstIsOdd = firstArray.some(element => element % 2 !== 0);
+
+const anyElementInSecondIsEven = secondArray.some(element => element % 2 === 0);
+const anyElementInSecondIsOdd = secondArray.some(element => element % 2 !== 0);
+
+const anyElementInThirdIsEven = thirdArray.some(element => element % 2 === 0);
+const anyElementInThirdIsOdd = thirdArray.some(element => element % 2 !== 0);
