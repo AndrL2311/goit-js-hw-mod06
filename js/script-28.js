d9ac2ccd07556cec31acf39a080e6349a3bebc2d
Andrii Lypovetskyi <li_andr@ukr.net>
2021-06-16 13:57:08 +0300

    Tasks24-27

---
+++ js/script-28.js
@@ -0,0 +1,35 @@
+// Задание
+// Используя метод some() дополни код так, чтобы:
+
+// В переменной anyElementInFirstIsEven был результат проверки наличия чётных элементов
+// в массиве firstArray.
+// В переменной anyElementInFirstIsOdd был результат проверки наличия нечётных элементов
+// в массиве firstArray.
+// В переменной anyElementInSecondIsEven был результат проверки наличия чётных элементов
+// в массиве secondArray.
+// В переменной anyElementInSecondIsOdd был результат проверки наличия нечётных элементов
+// в массиве secondArray.
+// В переменной anyElementInThirdIsEven был результат проверки наличия чётных элементов
+// в массиве thirdArray.
+// В переменной anyElementInThirdIsOdd был результат проверки наличия нечётных элементов
+// в массиве thirdArray.
+// Тесты
+// Объявлена переменная firstArray.
+// Значение переменной firstArray это массив [26, 94, 36, 18].
+// Объявлена переменная secondArray.
+// Значение переменной secondArray это массив [17, 61, 23].
+// Объявлена переменная thirdArray.
+// Значение переменной thirdArray это массив [17, 26, 94, 61, 36, 23, 18].
+// Объявлена переменная anyElementInFirstIsEven.
+// Значение переменной anyElementInFirstIsEven это буль true.
+// Объявлена переменная anyElementInFirstIsOdd.
+// Значение переменной anyElementInFirstIsOdd это буль false.
+// Объявлена переменная anyElementInSecondIsEven.
+// Значение переменной anyElementInSecondIsEven это буль false.
+// Объявлена переменная anyElementInSecondIsOdd.
+// Значение переменной anyElementInSecondIsOdd это буль true.
+// Объявлена переменная anyElementInThirdIsEven.
+// Значение переменной anyElementInThirdIsEven это буль true.
+// Объявлена переменная anyElementInThirdIsOdd.
+// Значение переменной anyElementInThirdIsOdd это буль true.
+// Для перебора массивов использован метод some().
